d278d97dd683b83d32c2d88871de77583c5fbe2a
Quentin <qug@hotmail.fr>
2021-10-23 17:21:27 +0200

    Created Spring Boot project

---
+++ src/main/java/fr/stack/grosmanginvo/ascastdemo/AsCastDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.stack.grosmanginvo.ascastdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsCastDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsCastDemoApplication.class, args);
+	}
+
+}
